5c4a81249d1208ce5865ae102b1c357a6aa2643a
Qualiss0 <boragurses05@gmail.com>
2026-09-08 20:32:01 +0200

    Exploratory data analysis and visualizations

--- coffee_sales_analysis.R
+++ coffee_sales_analysis.R
@@ -18,3 +18,25 @@ coffee$weekday <- weekdays(coffee$date)
 coffee$day_type <- ifelse(coffee$weekday %in% c("Saturday", "Sunday"), "Weekend", "Weekday")
 coffee$hour <- as.numeric(format(coffee$datetime, "%H"))
 str(coffee)
+
+summary(coffee$money)
+sd(coffee$money)
+
+tapply(coffee$money, coffee$cash_type, mean)
+tapply(coffee$money, coffee$cash_type, sd)
+
+sort(table(coffee$coffee_name), decreasing = TRUE)
+table(coffee$hour)
+
+
+boxplot(money ~ cash_type, data = coffee,
+        main = "Spending by Payment Type",
+        xlab = "Payment Type", ylab = "Amount")
+
+hist(coffee$money,
+     main = "Distribution of Transaction Amount",
+     xlab = "Amount", col = "lightblue")
+
+barplot(sort(table(coffee$coffee_name), decreasing = TRUE),
+        main = "Number of Sales by Coffee Type",
+        las = 2, col = "coral")
